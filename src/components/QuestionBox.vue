3a0f32975e1ff608883e71e4068634a1436d04b6
Freanu Peria <magnussonfrancis@gmail.com>
2020-01-24 17:25:53 +0800

    fixed showing of answers on QuestionBox.

--- src/components/QuestionBox.vue
+++ src/components/QuestionBox.vue
@@ -7,7 +7,7 @@
                 <hr class="my-4">
                 <b-list-group>
                     <b-list-group-item
-                         v-for="(answer, index) in answers" 
+                         v-for="(answer, index) in shuffledAnswers" 
                          :key="index"
                          @click="selectAnswer(index)"
                          :class="[selectedIndex === index ? 'selected' : '']"
@@ -16,7 +16,12 @@
                     </b-list-group-item>
                 </b-list-group>
 
-                <b-button variant="primary" href="#">Submit</b-button>
+                <b-button variant="primary"
+                    
+                >
+                <!-- ADD submitAnswer() method -->
+                    Submit
+                </b-button>
                 <b-button variant="success" class="button-next"
                     @click="nextQuestion"
                 >Next</b-button>
@@ -25,6 +30,8 @@
 </template>
 
 <script>
+import _ from 'lodash';
+
 export default {
     props: {
         currentQuestion: Object,
@@ -33,6 +40,7 @@ export default {
     data: function() {
         return {
             selectedIndex: null,
+            shuffledAnswers: []
         };
     },
     computed: {
@@ -42,11 +50,31 @@ export default {
             return answers;
         },
     },
+    watch : {
+        currentQuestion: {
+            immediate: true,
+            handler() {
+                this.selectedIndex = null;
+                this.shuffleAnswers();
+            }
+        }
+        // 
+        // currentQuestion() {
+        //     this.selectedIndex = null;
+        //     this.shuffleAnswers();
+        // }
+        // we'll make the currentQuestion into an object having options, so that we can shuffle answers right away
+    },
     methods: {
         selectAnswer(index) {
             this.selectedIndex = index;
+        },
+        shuffleAnswers() {
+            let answers = [...this.currentQuestion.incorrect_answers, this.currentQuestion.correct_answer];
+                this.shuffledAnswers = _.shuffle(answers);
+             
         }
-    }
+    },
 }
 </script>
 
