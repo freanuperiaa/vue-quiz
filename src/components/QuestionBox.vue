7794d6bb9996cad2f0083e27db60f89fc6af24d4
Freanu Peria <magnussonfrancis@gmail.com>
2020-01-24 17:06:07 +0800

    initial commit.

---
+++ src/components/QuestionBox.vue
@@ -0,0 +1,73 @@
+<template>
+    <div class="question-box-container">
+        <b-jumbotron lead="Bootstrap v4 Components for Vue.js 2">
+                <template v-slot:lead>
+                {{ currentQuestion.question }}
+                </template>
+                <hr class="my-4">
+                <b-list-group>
+                    <b-list-group-item
+                         v-for="(answer, index) in answers" 
+                         :key="index"
+                         @click="selectAnswer(index)"
+                         :class="[selectedIndex === index ? 'selected' : '']"
+                    >
+                        {{ answer }}
+                    </b-list-group-item>
+                </b-list-group>
+
+                <b-button variant="primary" href="#">Submit</b-button>
+                <b-button variant="success" class="button-next"
+                    @click="nextQuestion"
+                >Next</b-button>
+        </b-jumbotron>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        currentQuestion: Object,
+        nextQuestion: Function,
+    },
+    data: function() {
+        return {
+            selectedIndex: null,
+        };
+    },
+    computed: {
+        answers() {
+            let answers = [...this.currentQuestion.incorrect_answers];
+            answers.push(this.currentQuestion.correct_answer);
+            return answers;
+        },
+    },
+    methods: {
+        selectAnswer(index) {
+            this.selectedIndex = index;
+        }
+    }
+}
+</script>
+
+<style scoped>
+    .list-group {
+        margin-bottom: 15px;
+    }
+    .list-group-item:hover {
+        background: #EEE;
+        cursor: pointer;
+    }
+    .button-next {
+        margin-left: 15px;
+    }
+    .selected {
+        background-color: lightblue;
+    }
+    .correct {
+        background-color: lightgreen;
+    }
+    .incorrect {
+        background-color: lightcoral;
+    }
+</style>
